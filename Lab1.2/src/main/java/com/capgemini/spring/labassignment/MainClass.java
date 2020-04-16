c7639874b0364f59dfd6f560f16a9a152a9b0711
Rtvik Gupta <35890433+rtvikgupta@users.noreply.github.com>
2020-04-17 02:15:34 +0530

    Add files via upload

---
+++ Lab1.2/src/main/java/com/capgemini/spring/labassignment/MainClass.java
@@ -0,0 +1,17 @@
+package com.capgemini.spring.labassignment;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
+		
+		EmployeeModel emp = context.getBean(EmployeeModel.class);
+		SBU sbu = context.getBean(SBU.class);
+		
+		System.out.println(emp);
+		emp.showDeatils();
+	}
+}
